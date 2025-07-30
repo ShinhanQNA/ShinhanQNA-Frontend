31f3515b8c28b739f85b3bdfa755726c851c202e
김도훈 <dohun0310@naver.com>
2025-07-30 22:10:22 +0900

    FIX: Tab 컴포넌트에서 불필요한 상태 및 스타일 제거

--- components/Tab/index.tsx
+++ components/Tab/index.tsx
@@ -1,42 +1,18 @@
-"use client";
-
-import { useRef, useEffect, useState } from "react";
 import { TabProps } from "@/types/tab";
 import styles from "./tab.module.css";
 
 export default function Tab({ tabs, focusedTab, onTabClick }: TabProps) {
-  const tabRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const [underlineStyle, setUnderlineStyle] = useState({});
-
-  useEffect(() => {
-    const focusedIndex = tabs.findIndex((tab) => tab === focusedTab);
-    const focusedTabRef = tabRefs.current[focusedIndex];
-
-    if (focusedTabRef) {
-      setUnderlineStyle({
-        left: focusedTabRef.offsetLeft,
-        width: focusedTabRef.offsetWidth,
-      });
-    }
-  }, [focusedTab, tabs]);
-
   return (
     <div className={styles.tab}>
-      {tabs.map((tab, index) => (
+      {tabs.map((tab) => (
         <div
           key={tab}
-          ref={(el) => {
-            tabRefs.current[index] = el;
-          }}
-          className={`${styles.item} ${
-            focusedTab === tab ? styles.focused : ""
-          }`}
+          className={`${styles.item} ${focusedTab === tab ? styles.focused : ""}`}
           onClick={() => onTabClick(tab)}
         >
           {tab}
         </div>
       ))}
-      <div className={styles.underline} style={underlineStyle} />
     </div>
   );
 }
