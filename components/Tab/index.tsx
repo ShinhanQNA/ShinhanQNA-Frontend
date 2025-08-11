cb10d1b2ce9d1f0fd0f2a3e39fb186e274fca548
김도훈 <dohun0310@naver.com>
2025-08-11 14:54:32 +0900

    Fix: Tab 컴포넌트의 props 포맷팅 개선

--- components/Tab/index.tsx
+++ components/Tab/index.tsx
@@ -1,7 +1,11 @@
 import { TabProps } from "@/types/tab";
 import styles from "./tab.module.css";
 
-export default function Tab({ tabs, focusedTab, onTabClick }: TabProps) {
+export default function Tab({
+  tabs,
+  focusedTab,
+  onTabClick
+}: TabProps) {
   return (
     <div className={styles.tab}>
       {tabs.map((tab) => (
