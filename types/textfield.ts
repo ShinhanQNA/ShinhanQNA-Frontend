a3faac37104bd5221cd2b5886ac89af213adcd1b
김도훈 <dohun0310@naver.com>
2025-08-12 19:47:56 +0900

    Chore: TextFieldProps에서 InputHTMLAttributes를 React에서 가져오도록 수정

--- types/textfield.ts
+++ types/textfield.ts
@@ -1,6 +1,4 @@
-import type { InputHTMLAttributes } from "react";
-
-export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   helperText?: string;
   error?: boolean;
