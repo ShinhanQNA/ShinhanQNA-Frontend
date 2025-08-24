b789ef3507b379fccbcdc86e6581e9d9dd0a51a2
김도훈 <dohun0310@naver.com>
2025-08-25 01:08:47 +0900

    [DESIGN/#26] Feat: 답변 게시물 가져오기 API 및 타입 정의 추가

---
+++ types/answerpost.ts
@@ -0,0 +1,7 @@
+export default interface AnswerPosts {
+  postId: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
---
+++ utils/post/answer.ts
@@ -0,0 +1,25 @@
+import AnswerPosts from "@/types/answerpost";
+
+export default async function GetAnswerPosts(accessToken: string): Promise<AnswerPosts[]> {
+  if (!accessToken) {
+    throw new Error("unauthorized");
+  }
+
+  const backendUrl = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/answers`;
+  if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) throw new Error("server_misconfigured");
+
+  const res = await fetch(backendUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!res.ok) {
+    throw new Error("fetch_failed");
+  }
+
+  const data: AnswerPosts[] = await res.json();
+  return data;
+}
