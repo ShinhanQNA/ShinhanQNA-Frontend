e6aa705f64b56644a48c607766bfc209e8e683c9
김도훈 <dohun0310@naver.com>
2025-08-31 03:50:15 +0900

    [DESIGN/#26] Feat: MainList 컴포넌트 추가

---
+++ components/MainList/index.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import Tab from "@/components/Tab";
+import PostBox from "@/components/PostBox";
+import PostList from "@/types/postlist";
+import ThreeWeekGroup from "@/types/threeweekgroup";
+import AnswerList from "@/types/answerlist";
+
+function GeneralPosts({ posts }: { posts: PostList[] }) {
+  return (
+    <>
+      {posts.map((post) => (
+        <PostBox type="post" key={post.postId} slug={post.postId} title={post.title} content={post.content} />
+      ))}
+    </>
+  );
+}
+
+function SelectedGroup({ group }: { group: ThreeWeekGroup[] }) {
+  const year = new Date().getFullYear();
+
+  return (
+    <>
+      {group.map((item) => (
+        <PostBox
+          type="selected"
+          key={item.groupId}
+          slug={item.groupId}
+          title={`${year}년 ${item.selectedMonth}월`}
+          status={item.responseStatus}
+        />
+      ))}
+    </>
+  );
+}
+
+function AnswerPosts({ answers }: { answers: AnswerList[] }) {
+  return (
+    <>
+      {answers.map((answer) => (
+        <PostBox
+          path="answers"
+          type="notice"
+          key={answer.id}
+          slug={answer.id}
+          title={answer.title}
+          content={answer.content}
+        />
+      ))}
+    </>
+  );
+}
+
+export default function MainList({
+  posts,
+  group,
+  answers
+}: {
+  posts: PostList[];
+  group: ThreeWeekGroup[];
+  answers: AnswerList[];
+}) {
+  const tabs = ["📣 말해봐요", "⭐️ 선정된 의견", "✅ 답변 왔어요"];
+  const [tab, setTab] = useState<string>(tabs[0]);
+
+  return (
+    <>
+      <Tab tabs={tabs} focusedTab={tab} onTabClick={(t) => t !== tab && setTab(t)} />
+      {tab === tabs[0] && <GeneralPosts posts={posts} />}
+      {tab === tabs[1] && <SelectedGroup group={group} />}
+      {tab === tabs[2] && <AnswerPosts answers={answers} />}
+    </>
+  );
+}
