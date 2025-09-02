7260cc4e40119661fd6cce2bf4fcfb43524785f3
김도훈 <dohun0310@naver.com>
2025-09-02 13:22:17 +0900

    [DESIGN/#26] Feat: 내가 쓴 게시물 목록을 가져오는 함수 GetMyPostList 추가

---
+++ utils/post/mine.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import GetCookie from "../cookie/get";
+import PostList from "@/types/postlist";
+
+export default async function GetMyPostList(): Promise<
+  PostList[]
+> {
+  const backendUrl = `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/boards/my`;
+  if (!process.env.NEXT_PUBLIC_BACKEND_BASE_URL) throw new Error("server_misconfigured");
+
+  const accessToken = await GetCookie("access_token");
+  if (!accessToken) throw new Error("unauthorized");
+
+  const res = await fetch(backendUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    }
+  });
+  return res.json();
+}
