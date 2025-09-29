8fa76cbb00ce2562a656df39e0afece50056a13f
김도훈 <dohun0310@naver.com>
2025-09-29 17:37:20 +0900

    [DESIGN/#26] Feat: `Select` 컴포넌트에 레이블 추가 및 스타일 정의

--- types/select.ts
+++ types/select.ts
@@ -4,6 +4,7 @@ export default interface SelectProps {
   options: SelectOption[];
   value: SelectOption | null;
   onChange: (value: SelectOption | null) => void;
+  label?: string;
   placeholder?: string;
   variant?: "default" | "transparent";
   className?: string;
