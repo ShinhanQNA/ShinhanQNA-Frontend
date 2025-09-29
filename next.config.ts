5281cd13eb9a344a6f35aa6ade77379e93d9b17b
김도훈 <dohun0310@naver.com>
2025-09-29 14:45:50 +0900

    [DESIGN/#26] Setting:  버킷 URL을 가리기 위해 rewrite 사용

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/images/:path*",
+        destination: "https://" + process.env.S3_URL + "/:path*",
+      },
+    ];
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb"
+    }
+  }
 };
 
 export default nextConfig;
