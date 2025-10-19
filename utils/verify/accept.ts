0550e95f46ed2623d1203ed475f8c6cfdc82ac89
김도훈 <dohun0310@naver.com>
2025-10-19 16:28:33 +0900

    [DESIGN/#27] Refactor: `Verify` 승인, 거절 API 유틸 함수명 변경

--- utils/verify/accept.ts
+++ utils/verify/accept.ts
@@ -3,7 +3,7 @@
 import { redirect } from "next/navigation";
 import GetCookie from "../cookie/get";
 
-export default async function AcceptStudent(
+export default async function AcceptVerify(
   email: string
 ) {
   const backendUrl = `${process.env.BACKEND_BASE_URL}/admin/users/status`;
@@ -23,7 +23,7 @@ export default async function AcceptStudent(
       status: "가입 완료"
     })
   });
-  if (!res.ok) throw new Error("failed_to_accept_student");
+  if (!res.ok) throw new Error("failed_to_accept_verify");
 
   // 서버에서 바로 리다이렉트
   redirect("/verireq");
--- utils/verify/deny.ts
+++ utils/verify/deny.ts
@@ -3,7 +3,7 @@
 import { redirect } from "next/navigation";
 import GetCookie from "../cookie/get";
 
-export default async function DenyStudent(
+export default async function DenyVerify(
   email: string
 ) {
   const backendUrl = `${process.env.BACKEND_BASE_URL}/admin/users/status`;
@@ -23,7 +23,7 @@ export default async function DenyStudent(
       status: "가입 거절"
     })
   });
-  if (!res.ok) throw new Error("failed_to_deny_student");
+  if (!res.ok) throw new Error("failed_to_deny_verify");
 
   // 서버에서 바로 리다이렉트
   redirect("/verireq");
